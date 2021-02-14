d5cabede41b1243d5d1792ace05c9d109a45439e
Rtlockwood-O <rtlockwoodwork@gmail.com>
2021-02-13 20:10:27 -0700

    almost complete, play button does not show

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -30,12 +30,15 @@ export default function Signup() {
                         photoURL: Math.floor(Math.random() * 5) + 1,
                     })
                     .then(() => {
-                        setEmailAddress('')
-                        setPassword('')
-                        setError('')
+
                         history.push(ROUTES.BROWSE)
                     })
-            ).catch((error) => setError(error.message))
+            ).catch((error) => {
+                setEmailAddress('')
+                setPassword('')
+                setFirstName('')
+                setError(error.message)
+            })
     }
     return (
         <>
